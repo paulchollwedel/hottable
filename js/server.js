8d60f8e737f9669170ff4b7794e23dee3ede207e
Paul Hollwedel <paulchollwedel@gmail.com>
2019-12-14 11:29:14 -0800

    creating server

---
+++ js/server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = 8080;
+
+
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
+
+
+app.listen(PORT, function(){
+    console.log("App listening on PORT: " + PORT)
+});
