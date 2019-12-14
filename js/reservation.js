1c30833939c30111729d54b3ddda15bd71743a0f
Paul Hollwedel <paulchollwedel@gmail.com>
2019-12-14 11:56:59 -0800

    json tables added

---
+++ js/reservation.js
@@ -0,0 +1,10 @@
+const reservationData = [{
+
+    customerName: "John Smith",
+    customerEmail: "johnsmith@gmail.com",
+    customerUserName: "johnsmith",
+    phoneNumber: "000-000-0000",
+
+}];
+
+module.exports = reservationData;
--- js/server.js
+++ js/server.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const express = require("express");
 
 const app = express();
@@ -11,9 +10,9 @@ app.use(express.json());
 
 
 
+
 app.listen(PORT, function(){
     console.log("App listening on PORT: " + PORT)
 });
-=======
-//hello
->>>>>>> 3fc0266c2a30801857c5b834133b2a1d3b9c72b1
+
+
